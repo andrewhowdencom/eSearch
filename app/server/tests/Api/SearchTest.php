f116b8e0b1351ed0906660038da27adfd6645190
Andrew Howden <hello@andrewhowden.com>
2018-11-11 19:26:55 +0100

    Add a test for the invalid method for `/v1/search`
    Traditionally the author will implement E2E testing using something like
    Dredd. However, in this case it appears that Lumen provides superior
    testing primitives and it is trivial to stub the request and directly
    query the response data.
    
    This commit uses this to add a test for method not yet implemented.

---
+++ app/server/tests/Api/SearchTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Api;
+
+use Laravel\Lumen\Testing\TestCase;
+use \App\Entities\Status;
+
+class SearchTest extends TestCase
+{
+    /**
+     * Bootstraps the application
+     *
+     * @return mixed|\Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    public function createApplication()
+    {
+        return require __DIR__.'/../../bootstrap/app.php';
+    }
+
+    /**
+     * Checks that the application correctly rejects invalid methods.
+     */
+    public function testInvalidMethod()
+    {
+        $this->post('/v1/search');
+        $status = json_decode($this->response->getContent());
+
+        $this->assertEquals(Status::STATUS_FAILURE, $status->status);
+        $this->assertEquals(Status::REASON_METHOD_NOT_ALLOWED, $status->reason);
+    }
+}
