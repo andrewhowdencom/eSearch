1f08b251af62e1773bb12912e1df67e058d0ba90
Andrew Howden <hello@andrewhowden.com>
2018-11-19 21:13:58 +0100

    Add a help route
    By default, the application shows the Lumen version being run. This
    isn't particularly helpful
    
    This commit replaces that with a set of notes describing the app, how
    to use it and what to do.

--- app/server/routes/web.php
+++ app/server/routes/web.php
@@ -24,3 +24,36 @@ $router->get(
         'uses' => 'SearchController@search'
     ]
 );
+
+// Stub function that describes project purpose
+$router->get('/', function () {
+    $content = <<<EOF
+= eSearch API.
+
+You can find more information at:
+
+  - https://github.com/andrewhowdencom/esearch
+
+If you're playing around, you can:
+
+== Get a token
+
+Visit:
+
+  - https://api.esearch.local/auth/redirect
+
+== Query
+
+You will need a valid token set in a HTTP header in the following form:
+
+  Authentication: Bearer \${TOKEN}
+
+Then, visit:
+
+  - https://api.esearch.local/v1/search?q=Fish
+EOF;
+    $response = new \Illuminate\Http\Response($content);
+    $response->header('Content-type', 'text/plain');
+
+    return $response;
+});
